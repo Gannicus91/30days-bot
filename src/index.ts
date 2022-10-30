2c8d8ba9100be8b516007ef351ef68b9d7f5a7cd
Maxim Givanyan <givanyan99@yandex-team.ru>
2022-10-30 21:23:28 +0600

    add auth

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,83 @@
-import { Telegraf } from 'telegraf';
+import {Telegraf, Context as TelegrafContext} from 'telegraf';
+import botReplies from './bot-replies';
+import * as dotenv from 'dotenv';
+import isEmail from 'validator/lib/isEmail';
 
-const bot = new Telegraf(String(process.env.BOT_TOKEN));
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-void bot.launch();
+import { MongoClient } from 'mongodb';
+import { session } from 'telegraf-session-mongodb';
 
-// Enable graceful stop
+if (process.env.NODE_ENV !== 'production') {
+	dotenv.config();
+}
+
+interface ITelegrafSession {
+	userName?: string;
+	userEmail?: string;
+	stage?: number;
+}
+
+interface CustomContext extends TelegrafContext {
+	session: ITelegrafSession;
+}
+
+const bot = new Telegraf<CustomContext>(String(process.env.BOT_TOKEN));
+const mongoClient = new MongoClient(String(process.env.MONGO_URI));
+
+async function main(bot: Telegraf<CustomContext>, mongoClient: MongoClient) {
+	try {
+		await mongoClient.connect();
+		const db = mongoClient.db('sessionsDB');
+
+		bot.use(session(db, { collectionName: 'sessions' }));
+
+		bot.start(async (ctx) => {
+			if (!ctx.session.userName || !ctx.session.userEmail) {
+				await ctx.reply(botReplies.onStartHello);
+				await ctx.reply(botReplies.onStartInputName);
+			}
+		});
+
+		bot.command('remove', async (ctx) => {
+			await ctx.replyWithMarkdownV2(botReplies.removingUserData.replace('{{session}}', JSON.stringify(ctx.session)));
+			ctx.session = {};
+		});
+
+		bot.on('video', (ctx)=>{
+			console.log(ctx.message.video.file_id);
+		});
+
+		bot.on('text', async (ctx, next) => {
+			if (ctx.session.userName || ctx.session.userEmail) {
+				return next();
+			}
+
+			ctx.session.userName = ctx.message.text;
+			await ctx.reply(botReplies.onStartInputEmail);
+		});
+
+		bot.on('text', async (ctx, next) => {
+			if (!ctx.session.userName || ctx.session.userEmail) {
+				return next();
+			}
+
+			if (isEmail(ctx.message.text)) {
+				ctx.session.userEmail = ctx.message.text;
+				ctx.session.stage = 1;
+				await ctx.reply(botReplies.onStartAbout);
+				await ctx.reply(botReplies.onStartMotivation);
+				await ctx.replyWithVideo('BAACAgIAAxkBAAP3Y16TWwk72ORXPN_6DP8a1juqrqAAAqIdAALIH_lKWITCN_OlVUoqBA');
+			} else {
+				await ctx.reply(botReplies.emailError);
+			}
+		});
+		await bot.launch();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+
+void main(bot, mongoClient);
+
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
